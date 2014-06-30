a626792a10e1bcd852e3acd61682c2c399ea324d
JustinHue <justinhellsten@gmail.com>
2014-06-30 13:46:27 -0400

    Added jquery validation examples, and endofday countdown timer using jquery

---
+++ jquery_validation/registerform.php
@@ -0,0 +1,6 @@
+<?php
+	echo "Username: ".$_POST["username"]."<br/>";
+	echo "Email: ".$_POST["email"]."<br/>";
+	echo "Password: ".$_POST["password"]."<br/>";
+	echo "Confirm Password: ".$_POST["confirm_password"];
+?>
